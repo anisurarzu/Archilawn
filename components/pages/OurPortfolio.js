70fd5ea4ccc3abc5eceddef7425dcd74851b3a6e
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2024-08-11 20:38:21 +0600

    Mobile

--- components/pages/OurPortfolio.js
+++ components/pages/OurPortfolio.js
@@ -29,21 +29,21 @@ export default function OurPortfolio() {
 
         {/* START PORTFOLIO */}
         <div className="container mx-auto">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center px-1 md:px-0 lg:px-0">
             {/* Start 1st Image */}
-            <div className="relative w-[95vh] md:w-[30vh] lg:w-[95vh]">
+            <div className="relative w-[28vh] md:w-[30vh] lg:w-[95vh]">
               <Image
                 src={PortfolioImage1}
                 alt="PortfolioImage2"
-                className="h-[50vh] md:h-[20vh] lg:h-[50vh] w-full rounded-tl-[30px] rounded-bl-[30px] z-10"
+                className="h-[20vh] md:h-[20vh] lg:h-[50vh] w-full rounded-tl-[30px] rounded-bl-[30px] z-10"
               />
 
               {/* Overlay div */}
-              <div className="absolute bottom-0 w-full bg-[#8ABF55]/75 h-20 md:h-16 lg:h-20 flex items-center justify-between px-6 z-20 rounded-bl-[30px]">
-                <p className="text-white font-bold">
+              <div className="absolute bottom-0 w-full bg-[#8ABF55]/75 h-10 md:h-16 lg:h-20 flex items-center justify-between px-4 md:px-6 lg:px-6 z-20 rounded-bl-[30px]">
+                <p className="text-white font-bold text-[8px] md:text-base lg:text-base">
                   Food Truck Cafe | California
                 </p>
-                <button className="text-[#8ABF55] font-semibold bg-transparent border border-white py-1 md:py-0 lg:py-1 text-base md:text-sm lg:text-base px-2 md:px-2 lg:px-5 rounded-full bg-white">
+                <button className="text-[#8ABF55] font-semibold bg-transparent border border-white py-1 md:py-0 lg:py-1 text-[6px] md:text-sm lg:text-base px-2 md:px-2 lg:px-5 rounded-full bg-white">
                   SEE MORE
                 </button>
               </div>
@@ -51,19 +51,19 @@ export default function OurPortfolio() {
             {/* End 1st Image */}
 
             {/* Start 2nd Image */}
-            <div className="relative w-[68vh] md:w-[30vh] lg:w-[68vh]">
+            <div className="relative w-[23vh] md:w-[30vh] lg:w-[68vh]">
               <Image
                 src={PortfolioImage2}
                 alt="PortfolioImage2"
-                className="h-[50vh] md:h-[20vh] lg:h-[50vh] w-full rounded-br-[30px] rounded-tr-[30px] z-10"
+                className="h-[20vh] md:h-[20vh] lg:h-[50vh] w-full rounded-br-[30px] rounded-tr-[30px] z-10"
               />
 
               {/* Overlay div */}
-              <div className="absolute bottom-0 w-full bg-[#8ABF55]/75 h-20 md:h-16 lg:h-20 flex items-center justify-between px-6 z-20 rounded-br-[30px]">
-                <p className="text-white font-bold">
+              <div className="absolute bottom-0 w-full bg-[#8ABF55]/75 h-10 md:h-16 lg:h-20 flex items-center justify-between px-1 md:px-6 lg:px-6 z-20 rounded-br-[30px]">
+                <p className="text-white font-bold text-[8px] md:text-base lg:text-base">
                   Building Exterior Design | LA
                 </p>
-                <button className="text-[#8ABF55] font-semibold bg-transparent border border-white py-1 md:py-0 lg:py-1 text-base md:text-sm lg:text-base px-2 md:px-2 lg:px-5 rounded-full bg-white">
+                <button className="text-[#8ABF55] font-semibold bg-transparent border border-white py-1 md:py-0 lg:py-1 text-[6px] md:text-sm lg:text-base px-2 md:px-2 lg:px-5 rounded-full bg-white">
                   SEE MORE
                 </button>
               </div>
