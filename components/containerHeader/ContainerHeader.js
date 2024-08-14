12450fc48f3583899d7dada5f0a4895ea1165cca
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2024-08-14 13:56:21 +0600

    OK

--- components/containerHeader/ContainerHeader.js
+++ components/containerHeader/ContainerHeader.js
@@ -4,7 +4,7 @@ const ContainerHeader = ({ containerHeader }) => {
   return (
     <>
       <p
-        className="lg:text-[42px] md:text-[42px] text-[22px]"
+        className="lg:text-[42px] md:text-[42px] text-[22px] text-black"
         style={{
           fontFamily: "Tinos",
           lineHeight: "41px",
