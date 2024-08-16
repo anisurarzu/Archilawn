71d0de2f7176332f323061e239268c1bc0f7884f
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2024-08-17 01:05:05 +0600

    OK

---
+++ components/cards/ourServiceCard/OurServiceCardRight.js
@@ -0,0 +1,110 @@
+import Link from "next/link";
+import PackageText from "@/components/packageText/PackageText";
+import Image from "next/image";
+import React from "react";
+
+const OurServiceCardRight = ({
+  ServiceImage,
+  secondDivBgColor,
+  secondDivText,
+  thirdDivBgColor,
+  thirdDivHeaderText1,
+  thirdDivDescriptionText1,
+  thirdPackageColor1,
+  firsthrColor,
+  thirdDivHeaderText2,
+  thirdDivDescriptionText2,
+  thirdPackageColor2,
+  secondhrColor,
+  thirdDivHeaderText3,
+  thirdDivDescriptionText3,
+  thirdPackageColor3,
+  forthDivBgColor,
+}) => {
+  return (
+    <div className="h-[40vh] md:h-[30vh] lg:h-[40vh] flex relative group overflow-hidden">
+      {/* 4th */}
+      <div
+        className={`h-full rounded-[30px] flex justify-end items-center lg:ml-[1315px] md:ml-[250px] ml-[40px] lg:group-hover:ml-[545px] md:group-hover:ml-[32px] group-hover:ml-[45px] z-3 absolute transition-all duration-1000`}
+        style={{ backgroundColor: forthDivBgColor }}
+      >
+        <Link href="/serviceDetails">
+          <div className="flex items-end justify-start w-56 cursor-pointer">
+            <p
+              className="text-4xl font-bold text-white text-right pb-28 -rotate-90"
+              style={{
+                fontFamily: "Tinos",
+              }}
+            >
+              View Details
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      {/* 2nd */}
+      <div
+        className={`hidden h-full lg:flex justify-center items-center rounded-[30px] ml-[1400px] group-hover:ml-[650px] z-2 absolute transition-all duration-1000`}
+        style={{ backgroundColor: thirdDivBgColor }}
+      >
+        <div className="pr-36 pl-12">
+          <PackageText
+            headerText={thirdDivHeaderText1}
+            descriptionText={thirdDivDescriptionText1}
+            packingColor={thirdPackageColor1}
+          />
+
+          <hr
+            style={{ borderColor: firsthrColor, borderWidth: "1px" }}
+            className="my-6"
+          />
+
+          <PackageText
+            headerText={thirdDivHeaderText2}
+            descriptionText={thirdDivDescriptionText2}
+            packingColor={thirdPackageColor2}
+          />
+
+          <hr
+            style={{ borderColor: secondhrColor, borderWidth: "1px" }}
+            className="my-6"
+          />
+
+          {thirdDivHeaderText3 !== "" && (
+            <PackageText
+              headerText={thirdDivHeaderText3}
+              descriptionText={thirdDivDescriptionText3}
+              packingColor={thirdPackageColor3}
+            />
+          )}
+        </div>
+      </div>
+
+      {/* 1st */}
+      <div
+        className={`h-full flex justify-center items-center rounded-[30px] lg:ml-[1435px] md:ml-[335px] ml-[1435px] lg:group-hover:ml-[1200px] md:group-hover:ml-[110px] group-hover:ml-[1200px] z-5 absolute transition-all duration-1000`}
+        style={{ backgroundColor: secondDivBgColor }}
+      >
+        <p
+          className="text-4xl text-white pr-36 pl-[60px] lg:pl-[60px] md:pl-[45px]"
+          style={{
+            fontFamily: "Tinos",
+          }}
+          dangerouslySetInnerHTML={{ __html: secondDivText }} // Correct usage
+        />
+      </div>
+
+      <div className="flex justify-end items-end w-full">
+        <Image
+          src={ServiceImage}
+          alt="ServiceImageOne"
+          className="z-10 absolute h-full object-cover rounded-bl-[30px] rounded-tl-[30px]"
+        />
+      </div>
+
+      {/* end  */}
+    </div>
+  );
+};
+
+export default OurServiceCardRight;
---
+++ components/cards/ourServiceCard/OurServiceCardRightMobile.js
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import PackageText from "@/components/packageText/PackageText";
+import Image from "next/image";
+import React from "react";
+
+const OurServiceCardRightMobile = ({
+  ServiceImage,
+  secondDivBgColor,
+  secondDivText,
+  forthDivBgColor,
+}) => {
+  return (
+    <div className="h-[20vh] flex relative group">
+      {/* 3rd */}
+      <div
+        className={`h-full rounded-[30px] flex justify-end items-center ml-[185px] group-hover:ml-[60px] z-3 absolute transition-all duration-1000`}
+        style={{ backgroundColor: forthDivBgColor }}
+      >
+        <Link href="/detailsPages/DetailsPageOne">
+          <div className="flex items-start justify-start w-36 cursor-pointer">
+            <p
+              className="text-lg font-bold text-white text-right pb-14 -rotate-90"
+              style={{
+                fontFamily: "Tinos",
+              }}
+            >
+              View Details
+            </p>
+          </div>
+        </Link>
+      </div>
+
+      {/* 1st */}
+      <div
+        className={`h-full flex justify-center items-center rounded-[30px] ml-[230px] group-hover:ml-[110px] z-5 absolute transition-all duration-1000`}
+        style={{ backgroundColor: secondDivBgColor }}
+      >
+        <p
+          className="text-lg text-white pr-20 pl-6"
+          style={{
+            fontFamily: "Tinos",
+          }}
+          dangerouslySetInnerHTML={{ __html: secondDivText }} // Correct usage
+        />
+      </div>
+
+      <div className="flex justify-end items-end w-full">
+        <Image
+          src={ServiceImage}
+          alt="ServiceImageOne"
+          className="z-10 absolute h-full object-cover rounded-bl-[30px] rounded-tl-[30px] w-36"
+        />
+      </div>
+
+      {/* end  */}
+    </div>
+  );
+};
+
+export default OurServiceCardRightMobile;
