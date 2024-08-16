85fbf84a8bf42a60f7077ae3a0ef54a8d7031f7a
anisurarzu <anisur35-2163@diu.edu.bd>
2024-08-16 15:26:10 +0600

    new try

--- app/DetailsPageOne.js
+++ app/DetailsPageOne.js
@@ -1,20 +1,10 @@
-
-/* import React from "react";
-
+import React from "react";
 import Slider from "../pages/Slider";
 import OurService from "../pages/OurService";
 import PackageCard from "../cards/packageCard/PackageCard";
 import GalleryCard from "../cards/galleryCard/GalleryCard";
 import Footer from "@/components/pages/Footer";
-=======
-import GalleryCard from "@/components/cards/galleryCard/GalleryCard";
-import PackageCard from "@/components/cards/packageCard/PackageCard";
-import Footer from "@/components/pages/Footer";
 import Navbar from "@/components/pages/Navbar";
-import OurService from "@/components/pages/OurService";
-import Slider from "@/components/pages/Slider";
-import React from "react";
->>>>>>> 805e87398bfac2fa9eb2425a15b23d5657900c59
 
 const DetailsPageOne = () => {
   const ourServiceRef = useRef(null);
@@ -50,4 +40,3 @@ const DetailsPageOne = () => {
 };
 
 export default DetailsPageOne;
- */
