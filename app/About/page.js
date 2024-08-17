a9beb33a02a28a8117966e94246779c989f60cea
anisurarzu <anisur35-2163@diu.edu.bd>
2024-08-17 12:57:55 +0600

    fix

--- app/About/page.js
+++ app/About/page.js
@@ -1,5 +1,10 @@
+import AboutUs from "@/components/pages/about/AboutUs";
 import React from "react";
 
 export default function page() {
-  return <div></div>;
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
 }
